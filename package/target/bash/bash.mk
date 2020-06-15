927d9d0a671b57d01db9bbde2829720488eb4bc9
max_10 <max_10@gmx.de>
2020-06-15 20:11:56 +0200

    - bash: Patch changes

--- package/target/bash/bash.mk
+++ package/target/bash/bash.mk
@@ -6,13 +6,32 @@ BASH_DIR    = bash-$(BASH_VER)
 BASH_SOURCE = bash-$(BASH_VER).tar.gz
 BASH_URL    = http://ftp.gnu.org/gnu/bash
 
+BASH_PATCH = \
+	bash50-001 \
+	bash50-002 \
+	bash50-003 \
+	bash50-004 \
+	bash50-005 \
+	bash50-006 \
+	bash50-007 \
+	bash50-008 \
+	bash50-009 \
+	bash50-010 \
+	bash50-011 \
+	bash50-012 \
+	bash50-013 \
+	bash50-014 \
+	bash50-015 \
+	bash50-016 \
+	bash50-017
+
 $(D)/bash: bootstrap ncurses
 	$(START_BUILD)
 	$(call DOWNLOAD,$(PKG_SOURCE))
 	$(REMOVE)/$(PKG_DIR)
 	$(UNTAR)/$(PKG_SOURCE)
 	$(CHDIR)/$(PKG_DIR); \
-		$(call apply_patches, $(PKG_PATCHES_DIR), 0); \
+		$(call apply_patches, $(PKG_PATCH), 0); \
 		$(CONFIGURE); \
 		$(MAKE); \
 		$(INSTALL_EXEC) bash $(TARGET_DIR)/bin
