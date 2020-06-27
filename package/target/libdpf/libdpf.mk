090a1da7a70d303228a2058cb691131dd3ab90b7
max_10 <max_10@gmx.de>
2020-06-28 00:48:40 +0200

    - libdpf: update build

--- package/target/libdpf/libdpf.mk
+++ package/target/libdpf/libdpf.mk
@@ -14,13 +14,13 @@ $(D)/libdpf: bootstrap libusb-compat
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(PKG_REMOVE)
 	$(call PKG_UNPACK,$(BUILD_DIR))
-	$(PKG_CHDIR)/dpflib; \
+	$(PKG_CHDIR); \
 		$(call apply_patches, $(PKG_PATCH)); \
-		make libdpf.a CC=$(TARGET_CC) PREFIX=$(TARGET_DIR)/usr; \
+		make -C dpflib libdpf.a CC=$(TARGET_CC) PREFIX=$(TARGET_DIR)/usr; \
 		mkdir -p $(TARGET_INCLUDE_DIR)/libdpf; \
-		cp dpf.h $(TARGET_INCLUDE_DIR)/libdpf/libdpf.h; \
-		cp ../include/spiflash.h $(TARGET_INCLUDE_DIR)/libdpf/; \
-		cp ../include/usbuser.h $(TARGET_INCLUDE_DIR)/libdpf/; \
-		cp libdpf.a $(TARGET_LIB_DIR)/
+		cp dpflib/dpf.h $(TARGET_INCLUDE_DIR)/libdpf/libdpf.h; \
+		cp dpflib/libdpf.a $(TARGET_LIB_DIR)/; \
+		cp include/usbuser.h $(TARGET_INCLUDE_DIR)/libdpf/; \
+		cp include/spiflash.h $(TARGET_INCLUDE_DIR)/libdpf/
 	$(PKG_REMOVE)
 	$(TOUCH)
