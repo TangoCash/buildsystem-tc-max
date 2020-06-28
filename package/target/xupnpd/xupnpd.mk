022ffe13e7853a27f2dd243f1d191286c533846b
max_10 <max_10@gmx.de>
2020-06-28 19:41:52 +0200

    - update build xupnpd

--- package/target/xupnpd/xupnpd.mk
+++ package/target/xupnpd/xupnpd.mk
@@ -7,7 +7,7 @@ XUPNPD_SOURCE   = xupnpd.git
 XUPNPD_SITE     = https://github.com/clark15b
 XUPNPD_CHECKOUT = 25d6d44
 
-XUPNPD_PATCH  =  \
+XUPNPD_PATCH =  \
 	0001-xupnpd.patch
 
 $(D)/xupnpd: bootstrap lua openssl neutrino-plugins
@@ -22,12 +22,13 @@ $(D)/xupnpd: bootstrap lua openssl neutrino-plugins
 		$(MAKE) -C src embedded TARGET=$(TARGET) PKG_CONFIG=$(PKG_CONFIG) LUAFLAGS="$(TARGET_LDFLAGS) -I$(TARGET_INCLUDE_DIR)"; \
 		$(MAKE) -C src install DESTDIR=$(TARGET_DIR)
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/xupnpd.init $(TARGET_DIR)/etc/init.d/xupnpd
-	mkdir -p $(TARGET_DIR)/usr/share/xupnpd/{config,playlists}
-	rm $(TARGET_DIR)/usr/share/xupnpd/plugins/staff/xupnpd_18plus.lua
-	$(INSTALL_DATA) $(DL_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_18plus.lua ${TARGET_DIR}/usr/share/xupnpd/plugins/
-	$(INSTALL_DATA) $(DL_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_youtube.lua ${TARGET_DIR}/usr/share/xupnpd/plugins/
-	: $(INSTALL_DATA) $(DL_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_coolstream.lua ${TARGET_DIR}/usr/share/xupnpd/plugins/
-	$(INSTALL_DATA) $(DL_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_cczwei.lua ${TARGET_DIR}/usr/share/xupnpd/plugins/
-	$(UPDATE-RC.D) xupnpd defaults 50
+	mkdir -p $(TARGET_SHARE_DIR)/xupnpd/{config,playlists}
+	rm $(TARGET_SHARE_DIR)/xupnpd/plugins/staff/xupnpd_18plus.lua
+	$(INSTALL_DATA) -D $(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_18plus.lua $(TARGET_SHARE_DIR)/xupnpd/plugins/
+	$(INSTALL_DATA) -D $(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_cczwei.lua $(TARGET_SHARE_DIR)/xupnpd/plugins/
+	$(INSTALL_DATA) -D $(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_neutrino.lua $(TARGET_SHARE_DIR)/xupnpd/plugins/
+	$(INSTALL_DATA) -D $(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_vimeo.lua $(TARGET_SHARE_DIR)/xupnpd/plugins/
+	$(INSTALL_DATA) -D $(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/scripts-lua/xupnpd/xupnpd_youtube.lua $(TARGET_SHARE_DIR)/xupnpd/plugins/
+	$(UPDATE-RC.D) xupnpd defaults 75 25
 	$(PKG_REMOVE)
 	$(TOUCH)
