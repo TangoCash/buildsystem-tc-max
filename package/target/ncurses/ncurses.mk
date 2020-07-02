14602dece8257e737fe03d2f6e39766ea1d7337c
max_10 <max_10@gmx.de>
2020-07-02 22:11:40 +0200

    - ncurses: bump version to 6.1

--- package/target/ncurses/ncurses.mk
+++ package/target/ncurses/ncurses.mk
@@ -1,7 +1,7 @@
 #
 # ncurses
 #
-NCURSES_VER    = 6.0
+NCURSES_VER    = 6.1
 NCURSES_DIR    = ncurses-$(NCURSES_VER)
 NCURSES_SOURCE = ncurses-$(NCURSES_VER).tar.gz
 NCURSES_SITE   = https://ftp.gnu.org/pub/gnu/ncurses
@@ -39,10 +39,7 @@ $(D)/ncurses: bootstrap
 			--enable-const \
 			--enable-overwrite \
 			; \
-		$(MAKE) libs \
-			HOSTCC=gcc \
-			HOSTCCFLAGS="$(CFLAGS) -DHAVE_CONFIG_H -I../ncurses -DNDEBUG -D_GNU_SOURCE -I../include" \
-			HOSTLDFLAGS="$(LDFLAGS)"; \
+		$(MAKE) libs; \
 		$(MAKE) install.libs DESTDIR=$(TARGET_DIR)
 	mv $(TARGET_DIR)/usr/bin/ncurses6-config $(HOST_DIR)/bin
 	$(REWRITE_CONFIG) $(HOST_DIR)/bin/ncurses6-config
