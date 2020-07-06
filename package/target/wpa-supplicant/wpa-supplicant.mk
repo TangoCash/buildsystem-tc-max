cf015dd87cdaea22b0aacddd1b4852359d4aad80
max_10 <max_10@gmx.de>
2020-07-06 17:28:01 +0200

    - wpa-supplicant: delete unnecessary file

--- package/target/wpa-supplicant/wpa-supplicant.mk
+++ package/target/wpa-supplicant/wpa-supplicant.mk
@@ -16,16 +16,8 @@ $(D)/wpa-supplicant: bootstrap libnl openssl wireless-tools
 		$(BUILD_ENV) \
 		$(MAKE) -C wpa_supplicant; \
 		$(MAKE) -C wpa_supplicant install LIBDIR=/usr/lib BINDIR=/usr/sbin DESTDIR=$(TARGET_DIR)
-	mkdir -p $(TARGET_DIR)/etc/{network,wpa_supplicant}
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/post-wlan0.sh $(TARGET_DIR)/etc/network
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/pre-wlan0.sh $(TARGET_DIR)/etc/network
-	$(INSTALL_EXEC) $(PKG_FILES_DIR)/action_wpa.sh $(TARGET_DIR)/etc/wpa_supplicant
-	$(INSTALL_EXEC) $(PKG_FILES_DIR)/functions.sh $(TARGET_DIR)/etc/wpa_supplicant
-	$(INSTALL_EXEC) $(PKG_FILES_DIR)/ifupdown.sh $(TARGET_DIR)/etc/wpa_supplicant
 	$(INSTALL_DATA) $(PKG_FILES_DIR)/volatiles.99_wpa_supplicant $(TARGET_DIR)/etc/default/volatiles/99_wpa_supplicant
-	ln -sf ../../wpa_supplicant/ifupdown.sh $(TARGET_DIR)/etc/network/if-down.d/wpa-supplicant
-	ln -sf ../../wpa_supplicant/ifupdown.sh $(TARGET_DIR)/etc/network/if-post-down.d/wpa-supplicant
-	ln -sf ../../wpa_supplicant/ifupdown.sh $(TARGET_DIR)/etc/network/if-pre-up.d/wpa-supplicant
-	ln -sf ../../wpa_supplicant/ifupdown.sh $(TARGET_DIR)/etc/network/if-up.d/wpa-supplicant
 	$(PKG_REMOVE)
 	$(TOUCH)
