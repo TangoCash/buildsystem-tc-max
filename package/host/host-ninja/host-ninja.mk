8e1322cce9e8e4a82f99ac2db5682346c93c5cbf
max_10 <max_10@gmx.de>
2020-07-09 12:46:23 +0200

    - host-ninja: update github download

--- package/host/host-ninja/host-ninja.mk
+++ package/host/host-ninja/host-ninja.mk
@@ -4,8 +4,7 @@
 HOST_NINJA_VER    = 1.10.0
 HOST_NINJA_DIR    = ninja-$(HOST_NINJA_VER)
 HOST_NINJA_SOURCE = ninja-$(HOST_NINJA_VER).tar.gz
-HOST_NINJA_GIT    = v$(HOST_NINJA_VER).tar.gz -O $(DL_DIR)/$(HOST_NINJA_SOURCE)
-HOST_NINJA_SITE   = https://github.com/ninja-build/ninja/archive
+HOST_NINJA_SITE   = $(call github,ninja-build,ninja,v$(HOST_NINJA_VER))
 
 HOST_NINJA_PATCH = \
 	0001-set-minimum-cmake-version-to-3.10.patch \
@@ -15,7 +14,7 @@ HOST_NINJA_PATCH = \
 $(D)/host-ninja: bootstrap
 	$(START_BUILD)
 	$(PKG_REMOVE)
-	$(call PKG_DOWNLOAD,$(HOST_NINJA_GIT))
+	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
 		$(call apply_patches, $(PKG_PATCH)); \
