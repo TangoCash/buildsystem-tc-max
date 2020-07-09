ee09ba045f2f3e3dbdad9ef6f4600e35cdce4bb8
max_10 <max_10@gmx.de>
2020-07-09 12:49:48 +0200

    - slingshot: update github download

--- package/target/slingshot/slingshot.mk
+++ package/target/slingshot/slingshot.mk
@@ -4,10 +4,9 @@
 SLINGSHOT_VER    = 6
 SLINGSHOT_DIR    = slingshot-$(SLINGSHOT_VER)
 SLINGSHOT_SOURCE = slingshot-$(SLINGSHOT_VER).tar.gz
-SLINGSHOT_GIT    = v$(SLINGSHOT_VER).tar.gz -O $(DL_DIR)/$(SLINGSHOT_SOURCE)
-SLINGSHOT_SITE   = https://github.com/gvvaughan/slingshot/archive
+SLINGSHOT_SITE   = $(call github,gvvaughan,slingshot,v$(SLINGSHOT_VER))
 
 $(D)/slingshot: bootstrap
 	$(START_BUILD)
-	$(call PKG_DOWNLOAD,$(SLINGSHOT_GIT))
+	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(TOUCH)
