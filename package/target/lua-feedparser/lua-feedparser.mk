3cfa4389d6c9293a3369fcf72911a6dbc054c28c
max_10 <max_10@gmx.de>
2020-07-09 12:48:21 +0200

    - lua-feedparser: update github download

--- package/target/lua-feedparser/lua-feedparser.mk
+++ package/target/lua-feedparser/lua-feedparser.mk
@@ -4,13 +4,12 @@
 LUA_FEEDPARSER_VER    = 0.71
 LUA_FEEDPARSER_DIR    = lua-feedparser-$(LUA_FEEDPARSER_VER)
 LUA_FEEDPARSER_SOURCE = lua-feedparser-$(LUA_FEEDPARSER_VER).tar.gz
-LUA_FEEDPARSER_GIT    = $(LUA_FEEDPARSER_VER).tar.gz -O $(DL_DIR)/$(LUA_FEEDPARSER_SOURCE)
-LUA_FEEDPARSER_SITE   = https://github.com/slact/lua-feedparser/archive
+LUA_FEEDPARSER_SITE   = $(call github,slact,lua-feedparser,$(LUA_FEEDPARSER_VER))
 
 $(D)/lua-feedparser: bootstrap lua luasocket luaexpat
 	$(START_BUILD)
 	$(PKG_REMOVE)
-	$(call PKG_DOWNLOAD,$(LUA_FEEDPARSER_GIT))
+	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
 		sed -i -e "s/^PREFIX.*//" -e "s/^LUA_DIR.*//" Makefile ; \
