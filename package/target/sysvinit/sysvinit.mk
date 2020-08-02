a607a357568b6ee249322021ae321bc473239f33
max_10 <max_10@gmx.de>
2020-08-02 17:24:05 +0200

    - sysvinit: bump version to 2.97

--- package/target/sysvinit/sysvinit.mk
+++ package/target/sysvinit/sysvinit.mk
@@ -1,12 +1,13 @@
 #
 # sysvinit
 #
-SYSVINIT_VER    = 2.96
+SYSVINIT_VER    = 2.97
 SYSVINIT_DIR    = sysvinit-$(SYSVINIT_VER)
 SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VER).tar.xz
 SYSVINIT_SITE   = http://download.savannah.nongnu.org/releases/sysvinit
 
 SYSVINIT_PATCH  = \
+	0001-install.patch\
 	0001-crypt-lib.patch \
 	0002-change-INIT_FIFO.patch \
 	0003-pidof-add-m-option.patch \
@@ -21,7 +22,7 @@ $(D)/sysvinit: bootstrap
 		$(call apply_patches, $(PKG_PATCH)); \
 		$(BUILD_ENV) \
 		$(MAKE) SULOGINLIBS=-lcrypt; \
-		$(MAKE) install ROOT=$(TARGET_DIR) MANDIR=/.remove
+		$(MAKE) install ROOT=$(TARGET_DIR) mandir=/.remove
 	$(INSTALL_DATA) $(PKG_FILES_DIR)/inittab $(TARGET_DIR)/etc/inittab
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/autologin $(TARGET_DIR)/bin/autologin
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/rc $(TARGET_DIR)/etc/init.d
@@ -33,7 +34,7 @@ $(D)/sysvinit: bootstrap
 #	ln -sf bootlogd $(TARGET_DIR)/etc/init.d/stop-bootlogd
 #	$(UPDATE-RC.D) bootlogd start 07 S .
 #	$(UPDATE-RC.D) stop-bootlogd start 99 2 3 4 5 .
-	rm -f $(addprefix $(TARGET_DIR)/sbin/,fstab-decode runlevel telinit)
+	rm -f $(addprefix $(TARGET_DIR)/sbin/,fstab-decode logsave runlevel telinit)
 	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,lastb)
 	$(PKG_REMOVE)
 	$(TOUCH)
