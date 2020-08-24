85a6d204f8e3ae77b4e41b5639c5fe3129222ead
max_10 <max_10@gmx.de>
2020-08-24 21:58:38 +0200

    - lua: update build

--- package/target/lua/lua.mk
+++ package/target/lua/lua.mk
@@ -10,7 +10,7 @@ LUA_SITE   = https://www.lua.org/ftp
 LUA_PATCH  = \
 	0001-fix-lua-root.patch \
 	0002-remove-readline.patch \
-	0003-shared-library.patch \
+	0003-shared-libs-for-lua.patch \
 	0004-lua-pc.patch \
 	0005-crashfix.patch
 
@@ -22,16 +22,17 @@ $(D)/lua: bootstrap host-lua ncurses
 	$(PKG_CHDIR); \
 		$(call apply_patches, $(PKG_PATCH)); \
 		$(MAKE) linux \
+			BUILDMODE=dynamic \
 			PKG_VERSION=$(LUA_VER) \
 			$(MAKE_OPTS) \
 			AR="$(TARGET_AR) rcu" \
 			LDFLAGS="$(TARGET_LDFLAGS)" \
 			; \
 		$(MAKE) INSTALL_TOP=$(TARGET_DIR)/usr \
-			INSTALL_DATA="cp -d" \
 			INSTALL_MAN=$(TARGET_DIR)/.remove install \
-			TO_LIB="liblua.so liblua.so.$(LUA_ABIVER) liblua.so.$(LUA_VER)"
-	$(INSTALL_DATA) -D $(BUILD_DIR)/lua-$(LUA_VER)/etc/lua.pc $(PKG_CONFIG_PATH)/lua.pc
+			; \
+		$(INSTALL_DATA) -D $(BUILD_DIR)/lua-$(LUA_VER)/etc/lua.pc \
+			$(PKG_CONFIG_PATH)/lua.pc
 	rm -rf $(addprefix $(TARGET_DIR)/bin/,luac)
 	$(PKG_REMOVE)
 	$(TOUCH)
