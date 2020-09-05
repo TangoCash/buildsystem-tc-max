913529248e7903bcf18361115b6c55333c02463d
max_10 <max_10@gmx.de>
2020-09-06 00:47:36 +0200

    - ofgwrite: fixed build with -fno-common

--- package/target/ofgwrite/ofgwrite.mk
+++ package/target/ofgwrite/ofgwrite.mk
@@ -6,12 +6,16 @@ OFGWRITE_DIR    = ofgwrite-max.git
 OFGWRITE_SOURCE = ofgwrite-max.git
 OFGWRITE_SITE   = $(MAX-GIT-GITHUB)
 
+OFGWRITE_PATCH  = \
+	0001-fix-build-with-fno-common.patch
+
 $(D)/ofgwrite: bootstrap
 	$(START_BUILD)
 	$(PKG_REMOVE)
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
+		$(call apply_patches, $(PKG_PATCH)); \
 		$(BUILD_ENV) \
 		$(MAKE); \
 	$(INSTALL_EXEC) $(BUILD_DIR)/$(OFGWRITE_DIR)/ofgwrite_bin $(TARGET_DIR)/usr/bin
