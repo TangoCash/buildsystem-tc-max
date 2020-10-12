ca902e974d6b0df4a358ee9946ac29f9761a742e
max_10 <max_10@gmx.de>
2020-10-12 19:06:36 +0200

    - host-glib2: version bump 2.56.3 -> 2.62.4

--- package/host/host-glib2/host-glib2.mk
+++ package/host/host-glib2/host-glib2.mk
@@ -1,15 +1,12 @@
 #
 # host-glib2
 #
-HOST_GLIB2_VER    = 2.56.3
+HOST_GLIB2_VER    = 2.62.4
 HOST_GLIB2_DIR    = glib-$(HOST_GLIB2_VER)
 HOST_GLIB2_SOURCE = glib-$(HOST_GLIB2_VER).tar.xz
 HOST_GLIB2_SITE   = https://ftp.gnome.org/pub/gnome/sources/glib/$(basename $(HOST_GLIB2_VER))
 
-HOST_GLIB2_PATCH  = \
-	0004-gdbus-Avoid-printing-null-strings.patch
-
-$(D)/host-glib2: bootstrap host-libffi
+$(D)/host-glib2: bootstrap host-meson host-libffi
 	$(START_BUILD)
 	$(PKG_REMOVE)
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
@@ -17,16 +14,18 @@ $(D)/host-glib2: bootstrap host-libffi
 	$(PKG_CHDIR); \
 		export PKG_CONFIG=/usr/bin/pkg-config; \
 		export PKG_CONFIG_PATH=$(HOST_DIR)/lib/pkgconfig; \
-		$(call apply_patches, $(PKG_PATCH)); \
-		./configure $(SILENT_OPT) \
-			--prefix=`pwd`/out \
-			--enable-static=yes \
-			--enable-shared=no \
-			--disable-fam \
-			--disable-libmount \
-			--with-pcre=internal \
+		$(HOST_DIR)/bin/meson builddir/ --buildtype=release \
+			--prefix=/ \
+			-Ddtrace=false \
+			-Dfam=false \
+			-Dselinux=disabled \
+			-Dsystemtap=false \
+			-Dxattr=false \
+			-Dinternal_pcre=false \
+			-Dinstalled_tests=false \
+			-Doss_fuzz=disabled \
 			; \
-		$(MAKE) install; \
-		cp -a out/bin/glib-* $(HOST_DIR)/bin
+	$(PKG_CHDIR); \
+		DESTDIR=$(HOST_DIR) $(HOST_DIR)/bin/ninja -C builddir install
 	$(PKG_REMOVE)
 	$(TOUCH)
