3cba1d2f6d4cf2c8f94d86404c612a651cc8c2f7
max_10 <max_10@gmx.de>
2020-10-18 15:45:50 +0200

    - host-python3-setuptools: add 0002-change-shebang-to-python3.patch

--- package/host/host-python3-setuptools/host-python3-setuptools.mk
+++ package/host/host-python3-setuptools/host-python3-setuptools.mk
@@ -7,7 +7,8 @@ HOST_PYTHON3_SETUPTOOLS_SOURCE = setuptools-$(HOST_PYTHON3_SETUPTOOLS_VER).zip
 HOST_PYTHON3_SETUPTOOLS_SITE   = https://files.pythonhosted.org/packages/b0/f3/44da7482ac6da3f36f68e253cb04de37365b3dba9036a3c70773b778b485
 
 HOST_PYTHON3_SETUPTOOLS_PATCH = \
-	0001-add-executable.patch
+	0001-add-executable.patch \
+	0002-change-shebang-to-python3.patch
 
 $(D)/host-python3-setuptools: bootstrap host-python3
 	$(START_BUILD)
