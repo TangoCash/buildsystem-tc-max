6ef2869f63fc66d77f23e6b802296c0aa0ed485e
max_10 <max_10@gmx.de>
2020-11-09 22:46:14 +0100

    - ethtool: version bump 5.7 -> 5.9

--- package/target/ethtool/ethtool.mk
+++ package/target/ethtool/ethtool.mk
@@ -1,13 +1,10 @@
 #
 # ethtool
 #
-ETHTOOL_VER    = 5.7
+ETHTOOL_VER    = 5.9
 ETHTOOL_DIR    = ethtool-$(ETHTOOL_VER)
 ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VER).tar.xz
 ETHTOOL_SITE   = https://www.kernel.org/pub/software/network/ethtool
-
-ETHTOOL_PATCH  = \
-	0001-netlink-fix-build-warnings.patch
 
 $(D)/ethtool: bootstrap
 	$(START_BUILD)
@@ -15,7 +12,6 @@ $(D)/ethtool: bootstrap
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
-		$(call apply_patches, $(PKG_PATCH)); \
 		$(CONFIGURE) \
 			--prefix=/usr \
 			--mandir=/.remove \
