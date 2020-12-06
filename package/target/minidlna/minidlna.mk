3d8f32f8e48c4e5b25ff9fedcd1074447a7c3212
max_10 <max_10@gmx.de>
2020-12-06 12:07:35 +0100

    - minidlna: version bump 1.2.1 -> 1.3.0

--- package/target/minidlna/minidlna.mk
+++ package/target/minidlna/minidlna.mk
@@ -1,14 +1,12 @@
 #
 # minidlna
 #
-MINIDLNA_VER    = 1.2.1
+MINIDLNA_VER    = 1.3.0
 MINIDLNA_DIR    = minidlna-$(MINIDLNA_VER)
 MINIDLNA_SOURCE = minidlna-$(MINIDLNA_VER).tar.gz
 MINIDLNA_SITE   = https://sourceforge.net/projects/minidlna/files/minidlna/$(MINIDLNA_VER)
 
-MINIDLNA_PATCH  = \
-	0001-minidlna.patch \
-	0002-fix-build-with-fno-common.patch
+MINIDLNA_PATCH  =
 
 $(D)/minidlna: bootstrap zlib sqlite libexif libjpeg-turbo libid3tag libogg libvorbis flac ffmpeg
 	$(START_BUILD)
@@ -20,9 +18,11 @@ $(D)/minidlna: bootstrap zlib sqlite libexif libjpeg-turbo libid3tag libogg libv
 		autoreconf -fi $(SILENT_OPT); \
 		$(CONFIGURE) \
 			--prefix=/usr \
+			--disable-static \
 			--localedir=/.remove/locale \
 			; \
 		$(MAKE); \
-		$(MAKE) install prefix=/usr DESTDIR=$(TARGET_DIR)
+		$(MAKE) install DESTDIR=$(TARGET_DIR)
+	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/minidlna.conf $(TARGET_DIR)/etc/minidlna.conf
 	$(PKG_REMOVE)
 	$(TOUCH)
