4656b1a49150368366180fad784915a8feed0c83
max_10 <max_10@gmx.de>
2020-12-06 18:05:21 +0100

    - libexif: add no_docs.patch

--- package/target/libexif/libexif.mk
+++ package/target/libexif/libexif.mk
@@ -6,17 +6,20 @@ LIBEXIF_DIR    = libexif-$(LIBEXIF_VER)
 LIBEXIF_SOURCE = libexif-$(LIBEXIF_VER).tar.xz
 LIBEXIF_SITE   = https://github.com/libexif/libexif/releases/download/libexif-$(subst .,_,$(LIBEXIF_VER))-release
 
+LIBEXIF_PATCH  = \
+	0001-no_docs.patch
+
 $(D)/libexif: bootstrap
 	$(START_BUILD)
 	$(PKG_REMOVE)
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
-		$(CONFIGURE) \
-			--prefix=/usr \
-			; \
+		$(call apply_patches, $(PKG_PATCH)); \
+		autoreconf -fi $(SILENT_OPT); \
+		$(CONFIGURE); \
 		$(MAKE); \
-		$(MAKE) install prefix=/usr DESTDIR=$(TARGET_DIR) datadir=/.remove docdir=/.remove
+		$(MAKE) install DESTDIR=$(TARGET_DIR)
 	$(REWRITE_LIBTOOL_LA)
 	$(PKG_REMOVE)
 	$(TOUCH)
