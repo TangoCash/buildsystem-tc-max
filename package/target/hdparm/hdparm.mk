ed19d3ecdc6bd91a3c7c56157b2e60f3f24efafd
max_10 <max_10@gmx.de>
2020-12-06 12:02:38 +0100

    - hdparm: version bump 9.58 -> 9.60

--- package/target/hdparm/hdparm.mk
+++ package/target/hdparm/hdparm.mk
@@ -1,10 +1,13 @@
 #
 # hdparm
 #
-HDPARM_VER    = 9.58
+HDPARM_VER    = 9.60
 HDPARM_DIR    = hdparm-$(HDPARM_VER)
 HDPARM_SOURCE = hdparm-$(HDPARM_VER).tar.gz
 HDPARM_SITE   = https://sourceforge.net/projects/hdparm/files/hdparm
+
+HDPARM_PATCH  = \
+	0001-cflags.patch
 
 $(D)/hdparm: bootstrap
 	$(START_BUILD)
@@ -12,6 +15,7 @@ $(D)/hdparm: bootstrap
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
+		$(call apply_patches, $(PKG_PATCH)); \
 		$(BUILD_ENV) \
 		$(MAKE) CROSS=$(TARGET_CROSS) all; \
 		$(MAKE) install DESTDIR=$(TARGET_DIR) mandir=/.remove
