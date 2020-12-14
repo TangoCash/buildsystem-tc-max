4c3342f4f44bbca28d68551dcd5cbc58a2ae3a15
max_10 <max_10@gmx.de>
2020-12-14 23:29:32 +0100

    - libao: add 0001-no-docs.patch

--- package/target/libao/libao.mk
+++ package/target/libao/libao.mk
@@ -6,12 +6,16 @@ LIBAO_DIR    = libao-$(LIBAO_VER)
 LIBAO_SOURCE = libao-$(LIBAO_VER).tar.gz
 LIBAO_SITE   = https://ftp.osuosl.org/pub/xiph/releases/ao
 
+LIBAO_PATCH  = \
+	0001-no-docs.patch
+
 $(D)/libao: bootstrap alsa-lib
 	$(START_BUILD)
 	$(PKG_REMOVE)
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
 	$(PKG_CHDIR); \
+		$(call apply_patches, $(PKG_PATCH)); \
 		$(CONFIGURE) \
 			--prefix=/usr \
 			--mandir=/.remove \
