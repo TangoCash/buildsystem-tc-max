f045696a99a1a233c36f91cca305301db881ef41
max_10 <max_10@gmx.de>
2021-01-10 17:17:36 +0100

    - readline: bump to version 8.1

--- package/target/readline/readline.mk
+++ package/target/readline/readline.mk
@@ -1,26 +1,27 @@
 #
 # readline
 #
-READLINE_VER    = 6.2
+READLINE_VER    = 8.1
 READLINE_DIR    = readline-$(READLINE_VER)
 READLINE_SOURCE = readline-$(READLINE_VER).tar.gz
 READLINE_SITE   = https://ftp.gnu.org/gnu/readline
 
 READLINE_CONF_OPTS = \
 	--datadir=$(REMOVE_datarootdir) \
-	bash_cv_must_reinstall_sighandlers=no \
-	bash_cv_func_sigsetjmp=present \
-	bash_cv_func_strcoll_broken=no \
-	bash_cv_have_mbstate_t=yes
+	--disable-install-examples \
+	bash_cv_func_sigsetjmp=yes \
+	bash_cv_wcwidth_broken=no
 
-$(D)/readline: bootstrap
+$(D)/readline: bootstrap ncurses
 	$(START_BUILD)
 	$(PKG_REMOVE)
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	$(call PKG_UNPACK,$(BUILD_DIR))
+	$(PKG_APPLY_PATCHES)
 	$(PKG_CHDIR); \
 		$(CONFIGURE); \
 		$(MAKE); \
 		$(MAKE) install DESTDIR=$(TARGET_DIR)
+	$(INSTALL_DATA) $(PKG_FILES_DIR)/inputrc $(TARGET_DIR)/etc/inputrc
 	$(PKG_REMOVE)
 	$(TOUCH)
