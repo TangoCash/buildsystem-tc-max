a11f14ca27ba40fac26b38f8d8eb5e72f94fc504
max_10 <max_10@gmx.de>
2021-01-16 20:43:15 +0100

    - add minisatip

---
+++ package/target/minisatip/minisatip.mk
@@ -0,0 +1,28 @@
+#
+# minisatip
+#
+MINISATIP_VER    = git
+MINISATIP_DIR    = minisatip.git
+MINISATIP_SOURCE = minisatip.git
+MINISATIP_SITE   = https://github.com/catalinii
+MINISATIP_DEPS   = bootstrap libdvbcsa openssl
+
+MINISATIP_CONF_OPTS = \
+	--enable-static \
+	--enable-enigma \
+	--disable-netcv
+
+$(D)/minisatip:
+	$(START_BUILD)
+	$(PKG_REMOVE)
+	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
+	$(call PKG_UNPACK,$(BUILD_DIR))
+	$(PKG_APPLY_PATCHES)
+	$(PKG_CHDIR); \
+		$(CONFIGURE); \
+		$(MAKE) EXTRA_CFLAGS="-pipe -Os -Wall -g0 -I$(TARGET_DIR)/usr/include"; \
+	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/minisatip $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d $(TARGET_SHARE_DIR)/minisatip
+	$(INSTALL_COPY) $(PKG_BUILD_DIR)/html $(TARGET_SHARE_DIR)/minisatip
+	$(PKG_REMOVE)
+	$(TOUCH)
