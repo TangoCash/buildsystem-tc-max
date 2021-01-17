9b27f37df1a75b4b6b426e444b3ea2a45fdcbc12
max_10 <max_10@gmx.de>
2021-01-17 16:35:35 +0100

    - add dvb-apps

---
+++ package/target/dvb-apps/dvb-apps.mk
@@ -0,0 +1,22 @@
+#
+# dvb-apps
+#
+DVB_APPS_VER    = git
+DVB_APPS_DIR    = dvb-apps.git
+DVB_APPS_SOURCE = dvb-apps.git
+DVB_APPS_SITE   = https://github.com/openpli-arm
+DVB_APPS_DEPS   = bootstrap
+
+$(D)/dvb-apps:
+	$(START_BUILD)
+	$(PKG_REMOVE)
+	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
+	$(call PKG_UNPACK,$(BUILD_DIR))
+	$(PKG_APPLY_PATCHES)
+	$(PKG_CHDIR); \
+		export PERL_USE_UNSAFE_INC=1; \
+		$(BUILD_ENV) \
+		$(MAKE) DESTDIR=$(TARGET_DIR); \
+		$(MAKE) DESTDIR=$(TARGET_DIR) install
+	$(PKG_REMOVE)
+	$(TOUCH)
--- package/target/minisatip/minisatip.mk
+++ package/target/minisatip/minisatip.mk
@@ -5,7 +5,7 @@ MINISATIP_VER    = git
 MINISATIP_DIR    = minisatip.git
 MINISATIP_SOURCE = minisatip.git
 MINISATIP_SITE   = https://github.com/catalinii
-MINISATIP_DEPS   = bootstrap libdvbcsa openssl
+MINISATIP_DEPS   = bootstrap libdvbcsa openssl dvb-apps
 
 MINISATIP_CONF_OPTS = \
 	--enable-static \
