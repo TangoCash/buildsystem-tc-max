5f1f7cfe576943fa6207d3b6d46064fd955ec529
max_10 <max_10@gmx.de>
2021-02-02 21:40:22 +0100

    - graphlcd-base: update custom patch

--- package/target/graphlcd-base/graphlcd-base.mk
+++ package/target/graphlcd-base/graphlcd-base.mk
@@ -8,17 +8,17 @@ GRAPHLCD_BASE_SITE   = git://projects.vdr-developer.org
 GRAPHLCD_BASE_DEPS   = bootstrap freetype libiconv libusb
 
 ifeq ($(FLAVOUR),$(filter $(FLAVOUR),neutrino-ni))
-GRAPHLCD_BASE_CUSTOM_PATCH += 0004-material-colors.patch
+GRAPHLCD_BASE_CUSTOM_PATCH += 0004-material-colors.patch.custom
 else
-GRAPHLCD_BASE_CUSTOM_PATCH += 0003-material-colors.patch
+GRAPHLCD_BASE_CUSTOM_PATCH += 0003-material-colors.patch.custom
 endif
 
 ifeq ($(BOXMODEL),$(filter $(BOXMODEL),vuduo4k vuduo4kse vusolo4k vuultimo4k vuuno4kse))
-GRAPHLCD_BASE_CUSTOM_PATCH += 0005-add-vuplus-driver.patch
+GRAPHLCD_BASE_CUSTOM_PATCH += 0005-add-vuplus-driver.patch.custom
 endif
 
 define GRAPHLCD_BASE_CUSTOM_PATCHES
-	$(APPLY_PATCHES) $(PKG_BUILD_DIR) $(PKG_PATCHES_DIR)/custom \$(GRAPHLCD_BASE_CUSTOM_PATCH)
+	$(APPLY_PATCHES) $(PKG_BUILD_DIR) $(PKG_PATCHES_DIR) $(GRAPHLCD_BASE_CUSTOM_PATCH)
 endef
 GRAPHLCD_BASE_POST_PATCH_HOOKS += GRAPHLCD_BASE_CUSTOM_PATCHES
 
