20bd1c6813825839a1586118bb5ef33d2a87a53d
max_10 <max_10@gmx.de>
2021-02-21 12:50:47 +0100

    - ca-bundle: fix url

--- package/target/ca-bundle/ca-bundle.mk
+++ package/target/ca-bundle/ca-bundle.mk
@@ -4,7 +4,7 @@
 CA_BUNDLE_VER    = 1.0
 CA_BUNDLE_DIR    = /etc/ssl/certs
 CA_BUNDLE_SOURCE = cacert.pem
-CA_BUNDLE_SITE   = https://curl.haxx.se/ca
+CA_BUNDLE_SITE   = https://curl.se/ca
 CA_BUNDLE_DEPS   = bootstrap
 
 CA_BUNDLE_CRT    = ca-certificates.crt
@@ -13,6 +13,6 @@ $(D)/ca-bundle:
 	$(START_BUILD)
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
 	cd $(DL_DIR); \
-		curl --silent --remote-name --time-cond $(CA_BUNDLE_SOURCE) $(CA_BUNDLE_SITE)/$(CA-BUNDLE_SOURCE) || true
+		curl --silent --remote-name --remote-time -z $(CA_BUNDLE_SOURCE) $(CA_BUNDLE_SITE)/$(CA-BUNDLE_SOURCE) || true
 	$(INSTALL_DATA) -D $(DL_DIR)/$(CA_BUNDLE_SOURCE) $(TARGET_DIR)/$(CA_BUNDLE_DIR)/$(CA_BUNDLE_CRT)
 	$(TOUCH)
