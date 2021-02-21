977b2c6f63d917d3f130c31c16699b0c28fd7af4
max_10 <max_10@gmx.de>
2021-02-21 23:54:24 +0100

    - hd61-libs: update build

--- package/machine/hd61-libs/hd61-libs.mk
+++ package/machine/hd61-libs/hd61-libs.mk
@@ -3,6 +3,7 @@
 #
 HD61_LIBS_DATE   = 20200622
 HD61_LIBS_VER    = $(HD61_LIBS_DATE)
+HD61_LIBS_DIR    = hiplay
 HD61_LIBS_SOURCE = hd61-libs-$(HD61_LIBS_VER).zip
 HD61_LIBS_SITE   = http://downloads.mutant-digital.net/hd61
 HD61_LIBS_DEPS   = bootstrap
@@ -11,7 +12,7 @@ $(D)/hd61-libs:
 	$(START_BUILD)
 	$(REMOVE)/hiplay
 	$(call PKG_DOWNLOAD,$(PKG_SOURCE))
-	$(call PKG_UNPACK,$(BUILD_DIR)/hiplay)
+	$(call PKG_UNPACK,$(PKG_BUILD_DIR))
 	mkdir -p $(TARGET_LIB_DIR)/hisilicon
 	$(INSTALL_EXEC) $(BUILD_DIR)/hiplay/hisilicon/* $(TARGET_LIB_DIR)/hisilicon
 	$(INSTALL_EXEC) $(BUILD_DIR)/hiplay/ffmpeg/* $(TARGET_LIB_DIR)/hisilicon
