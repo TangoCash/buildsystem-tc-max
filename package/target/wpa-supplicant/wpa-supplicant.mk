30b0f4d5e6df1f3e64519814ff16084ebe64b015
max_10 <max_10@gmx.de>
2021-03-06 12:42:50 +0100

    - wpa-supplicant: bump to version 2.9

--- package/target/wpa-supplicant/wpa-supplicant.mk
+++ package/target/wpa-supplicant/wpa-supplicant.mk
@@ -1,7 +1,7 @@
 #
 # wpa-supplicant
 #
-WPA_SUPPLICANT_VER    = 2.7
+WPA_SUPPLICANT_VER    = 2.9
 WPA_SUPPLICANT_DIR    = wpa_supplicant-$(WPA_SUPPLICANT_VER)
 WPA_SUPPLICANT_SOURCE = wpa_supplicant-$(WPA_SUPPLICANT_VER).tar.gz
 WPA_SUPPLICANT_SITE   = https://w1.fi/releases
@@ -17,9 +17,10 @@ $(D)/wpa-supplicant:
 		$(INSTALL_DATA) $(PKG_FILES_DIR)/wpa_supplicant.config wpa_supplicant/.config; \
 		$(TARGET_CONFIGURE_ENV) \
 		$(MAKE) -C wpa_supplicant; \
-		$(MAKE) -C wpa_supplicant install LIBDIR=$(base_libdir) BINDIR=$(sbindir) DESTDIR=$(TARGET_DIR)
+		$(MAKE) -C wpa_supplicant install LIBDIR=$(libdir) BINDIR=$(sbindir) DESTDIR=$(TARGET_DIR)
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/post-wlan0.sh $(TARGET_DIR)/etc/network
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/pre-wlan0.sh $(TARGET_DIR)/etc/network
 	$(INSTALL_DATA) $(PKG_FILES_DIR)/volatiles.99_wpa_supplicant $(TARGET_DIR)/etc/default/volatiles/99_wpa_supplicant
+	$(INSTALL_DATA) $(PKG_FILES_DIR)/wpa_supplicant.conf $(TARGET_DIR)/etc/wpa_supplicant.conf
 	$(REMOVE)
 	$(TOUCH)
