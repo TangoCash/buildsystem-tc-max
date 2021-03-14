d65595c7f06fce5d508fd981b8e614d487964fbc
max_10 <max_10@gmx.de>
2021-03-14 16:38:47 +0100

    - gptfdisk: bump to version 1.0.7

--- package/target/gptfdisk/gptfdisk.mk
+++ package/target/gptfdisk/gptfdisk.mk
@@ -1,7 +1,7 @@
 #
 # gptfdisk
 #
-GPTFDISK_VER    = 1.0.6
+GPTFDISK_VER    = 1.0.7
 GPTFDISK_DIR    = gptfdisk-$(GPTFDISK_VER)
 GPTFDISK_SOURCE = gptfdisk-$(GPTFDISK_VER).tar.gz
 GPTFDISK_SITE   = https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$(GPTFDISK_VER)
