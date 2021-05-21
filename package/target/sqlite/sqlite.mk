8e7b3ffc85333aceddc5923600c760a33e145700
max_10 <max_10@gmx.de>
2021-05-21 22:34:59 +0200

    - sqlite: bump to version 3350500

--- package/target/sqlite/sqlite.mk
+++ package/target/sqlite/sqlite.mk
@@ -1,11 +1,16 @@
 #
 # sqlite
 #
-SQLITE_VERSION = 3330000
+SQLITE_VERSION = 3350500
 SQLITE_DIR     = sqlite-autoconf-$(SQLITE_VERSION)
 SQLITE_SOURCE  = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
-SQLITE_SITE    = http://www.sqlite.org/2020
+SQLITE_SITE    = http://www.sqlite.org/2021
 SQLITE_DEPENDS = bootstrap
+
+SQLITE_CONF_OPTS = \
+	--enable-shared \
+	--enable-threadsafe \
+	--disable-static-shell
 
 $(D)/sqlite:
 	$(START_BUILD)
