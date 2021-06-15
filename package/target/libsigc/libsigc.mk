b9a318d268494d95e1d5935fa050ce8b234263f9
max_10 <max_10@gmx.de>
2021-06-15 16:38:56 +0200

    - libsigc++: bump to version 2.10.7

--- package/target/libsigc/libsigc.mk
+++ package/target/libsigc/libsigc.mk
@@ -1,7 +1,7 @@
 #
 # libsigc
 #
-LIBSIGC_VERSION = 2.10.6
+LIBSIGC_VERSION = 2.10.7
 LIBSIGC_DIR     = libsigc++-$(LIBSIGC_VERSION)
 LIBSIGC_SOURCE  = libsigc++-$(LIBSIGC_VERSION).tar.xz
 LIBSIGC_SITE    = https://download.gnome.org/sources/libsigc++/$(basename $(LIBSIGC_VERSION))
