8102f9f4da6d7e8e35d374ccd3333dc4890313b2
max_10 <max_10@gmx.de>
2021-07-04 15:03:38 +0200

    - host-meson: bump to version 0.58.1

--- package/host/host-meson/host-meson.mk
+++ package/host/host-meson/host-meson.mk
@@ -1,7 +1,7 @@
 #
 # host-meson
 #
-HOST_MESON_VERSION = 0.57.2
+HOST_MESON_VERSION = 0.58.1
 HOST_MESON_DIR     = meson-$(HOST_MESON_VERSION)
 HOST_MESON_SOURCE  = meson-$(HOST_MESON_VERSION).tar.gz
 HOST_MESON_SITE    = https://github.com/mesonbuild/meson/releases/download/$(HOST_MESON_VERSION)
