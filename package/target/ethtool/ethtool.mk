b659bbe22febf71157677752dc82e730ad9e9e5a
max_10 <max_10@gmx.de>
2021-09-30 11:59:25 +0200

    - ethtool: bump to version 5.14

--- package/target/ethtool/ethtool.mk
+++ package/target/ethtool/ethtool.mk
@@ -1,7 +1,7 @@
 #
 # ethtool
 #
-ETHTOOL_VERSION = 5.13
+ETHTOOL_VERSION = 5.14
 ETHTOOL_DIR     = ethtool-$(ETHTOOL_VERSION)
 ETHTOOL_SOURCE  = ethtool-$(ETHTOOL_VERSION).tar.xz
 ETHTOOL_SITE    = https://www.kernel.org/pub/software/network/ethtool
