f47cd9ad57b61c95835887eac226c19d40bee7ea
max_10 <max_10@gmx.de>
2021-09-30 12:01:13 +0200

    - fontconfig: bump to version 2.13.94

--- package/target/fontconfig/fontconfig.mk
+++ package/target/fontconfig/fontconfig.mk
@@ -1,9 +1,9 @@
 #
 # fontconfig
 #
-FONTCONFIG_VERSION = 2.11.93
+FONTCONFIG_VERSION = 2.13.94
 FONTCONFIG_DIR     = fontconfig-$(FONTCONFIG_VERSION)
-FONTCONFIG_SOURCE  = fontconfig-$(FONTCONFIG_VERSION).tar.bz2
+FONTCONFIG_SOURCE  = fontconfig-$(FONTCONFIG_VERSION).tar.xz
 FONTCONFIG_SITE    = https://www.freedesktop.org/software/fontconfig/release
 FONTCONFIG_DEPENDS = bootstrap freetype expat
 
