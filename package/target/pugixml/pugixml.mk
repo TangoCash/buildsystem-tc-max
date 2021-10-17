12a06875dcbc9f612cfed524ad312d7758c4389a
max_10 <max_10@gmx.de>
2021-10-17 15:57:05 +0200

    - pugixml: bump to version 1.11.1

--- package/target/pugixml/pugixml.mk
+++ package/target/pugixml/pugixml.mk
@@ -1,13 +1,14 @@
 #
 # pugixml
 #
-PUGIXML_VERSION = 1.10
+PUGIXML_VERSION = 1.11.1
 PUGIXML_DIR     = pugixml-$(PUGIXML_VERSION)
 PUGIXML_SOURCE  = pugixml-$(PUGIXML_VERSION).tar.gz
 PUGIXML_SITE    = https://github.com/zeux/pugixml/releases/download/v$(PUGIXML_VERSION)
 PUGIXML_DEPENDS = bootstrap
 
 PUGIXML_CONF_OPTS = \
+	-DBUILD_DEFINES="PUGIXML_HAS_LONG_LONG" \
 	| tail -n +90
 
 $(D)/pugixml:
