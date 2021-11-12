f08e4296e053d723df2e876f04e591710c98a0c2
max_10 <max_10@gmx.de>
2021-11-12 22:47:18 +0100

    - ffmpeg: bump version to 4.4.1

--- package/target/ffmpeg/ffmpeg.mk
+++ package/target/ffmpeg/ffmpeg.mk
@@ -1,11 +1,11 @@
 #
 # ffmpeg
 #
-FFMPEG_VERSION = 4.4
-FFMPEG_DIR     = ffmpeg-$(FFMPEG_VERSION)
-FFMPEG_SOURCE  = ffmpeg-$(FFMPEG_VERSION).tar.xz
-FFMPEG_SITE    = http://www.ffmpeg.org/releases
-FFMPEG_DEPENDS = bootstrap openssl zlib bzip2 freetype rtmpdump libass libxml2
+FFMPEG_VERSION ?= 4.4.1
+FFMPEG_DIR      = ffmpeg-$(FFMPEG_VERSION)
+FFMPEG_SOURCE   = ffmpeg-$(FFMPEG_VERSION).tar.xz
+FFMPEG_SITE     = http://www.ffmpeg.org/releases
+FFMPEG_DEPENDS  = bootstrap openssl zlib bzip2 freetype rtmpdump libass libxml2
 
 FFMPEG_CONF_OPTS = \
 	--disable-ffplay \
