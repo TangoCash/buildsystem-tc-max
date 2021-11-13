d60f026b9d7edcbf73aef4cdcafead7304dff5f3
max_10 <max_10@gmx.de>
2021-11-13 22:41:49 +0100

    - fribidi: bump version to 1.0.11

--- package/target/fribidi/fribidi.mk
+++ package/target/fribidi/fribidi.mk
@@ -1,7 +1,7 @@
 #
 # fribidi
 #
-FRIBIDI_VERSION = 1.0.10
+FRIBIDI_VERSION = 1.0.11
 FRIBIDI_DIR     = fribidi-$(FRIBIDI_VERSION)
 FRIBIDI_SOURCE  = fribidi-$(FRIBIDI_VERSION).tar.xz
 FRIBIDI_SITE    = https://github.com/fribidi/fribidi/releases/download/v$(FRIBIDI_VERSION)
