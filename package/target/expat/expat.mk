3230e4bf275a34b8b047ed9462e885e7fda2c897
max_10 <max_10@gmx.de>
2022-02-02 17:32:56 +0100

    - expat: bump version to 2.4.4

--- package/target/expat/expat.mk
+++ package/target/expat/expat.mk
@@ -1,13 +1,11 @@
 #
 # expat
 #
-EXPAT_VERSION = 2.4.3
+EXPAT_VERSION = 2.4.4
 EXPAT_DIR     = expat-$(EXPAT_VERSION)
 EXPAT_SOURCE  = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_SITE    = https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$(EXPAT_VERSION))
 EXPAT_DEPENDS = bootstrap
-
-EXPAT_AUTORECONF = YES
 
 EXPAT_CONF_OPTS = \
 	--docdir=$(REMOVE_docdir) \
