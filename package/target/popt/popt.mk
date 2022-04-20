395d5242e70a093ef60b5617f24cae25cb7db3f8
max_10 <max_10@gmx.de>
2022-04-20 17:06:54 +0200

    - popt: bump version to 1.18

--- package/target/popt/popt.mk
+++ package/target/popt/popt.mk
@@ -1,10 +1,10 @@
 #
 # popt
 #
-POPT_VERSION = 1.16
+POPT_VERSION = 1.18
 POPT_DIR     = popt-$(POPT_VERSION)
 POPT_SOURCE  = popt-$(POPT_VERSION).tar.gz
-POPT_SITE    = ftp://anduin.linuxfromscratch.org/BLFS/popt
+POPT_SITE    = http://ftp.rpm.org/popt/releases/popt-1.x
 POPT_DEPENDS = bootstrap
 
 POPT_CONF_OPTS = \
