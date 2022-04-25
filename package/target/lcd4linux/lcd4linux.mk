04c340cd304714b8ec2372c15635525236637c7e
max_10 <max_10@gmx.de>
2022-04-25 17:42:45 +0200

    - lcd4linux: sync with horsti58

--- package/target/lcd4linux/lcd4linux.mk
+++ package/target/lcd4linux/lcd4linux.mk
@@ -2,9 +2,9 @@
 # lcd4linux
 #
 LCD4LINUX_VERSION = git
-LCD4LINUX_DIR     = lcd4linux-max.git
-LCD4LINUX_SOURCE  = lcd4linux-max.git
-LCD4LINUX_SITE    = https://github.com/MaxWiesel
+LCD4LINUX_DIR     = lcd4linux.git
+LCD4LINUX_SOURCE  = lcd4linux.git
+LCD4LINUX_SITE    = https://github.com/TangoCash
 LCD4LINUX_DEPENDS = bootstrap $(SHARE_LCD4LINUX) ncurses libusb-compat libgd libusb libdpf
 
 LCD4LINUX_CONF_OPTS = \
@@ -25,6 +25,7 @@ $(D)/lcd4linux:
 		$(MAKE) install DESTDIR=$(TARGET_DIR)
 	cp -a $(PKG_FILES_DIR)/icons/* $(SHARE_LCD4LINUX)
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/lcd4linux.init $(TARGET_DIR)/etc/init.d/lcd4linux
+	$(INSTALL_CONF) $(PKG_FILES_DIR)/lcd4config.conf $(TARGET_DIR)/etc/lcd4config.conf
 	$(INSTALL_CONF) $(PKG_FILES_DIR)/lcd4linux.conf $(TARGET_DIR)/etc/lcd4linux.conf
 	$(INSTALL_CONF) $(PKG_FILES_DIR)/lcd4linux-var.conf $(TARGET_DIR)/var/etc/lcd4linux.conf
 	$(REMOVE)
