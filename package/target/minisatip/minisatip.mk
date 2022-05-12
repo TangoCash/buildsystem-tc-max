b9944b43456fcda56670db47da4b158bcef8708b
max_10 <max_10@gmx.de>
2022-05-12 10:03:23 +0200

    - minisatip: update build

--- package/target/minisatip/minisatip.mk
+++ package/target/minisatip/minisatip.mk
@@ -5,11 +5,13 @@ MINISATIP_VERSION = git
 MINISATIP_DIR     = minisatip.git
 MINISATIP_SOURCE  = minisatip.git
 MINISATIP_SITE    = https://github.com/catalinii
-MINISATIP_DEPENDS = bootstrap libdvbcsa openssl dvb-apps
+MINISATIP_DEPENDS = bootstrap libdvbcsa openssl
+
+MINISATIP_CONF_ENV = \
+	CFLAGS+=" -ldl"
 
 MINISATIP_CONF_OPTS = \
 	--enable-static \
-	--enable-enigma \
 	--disable-netcv
 
 $(D)/minisatip:
@@ -20,7 +22,8 @@ $(D)/minisatip:
 	$(APPLY_PATCHES)
 	$(CD_BUILD_DIR); \
 		$(CONFIGURE); \
-		$(MAKE) EXTRA_CFLAGS="-pipe -Os -Wall -g0 -I$(TARGET_DIR)/usr/include"; \
+		$(TARGET_CONFIGURE_OPTS) \
+		$(MAKE)
 	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/minisatip $(TARGET_DIR)/usr/bin
 	$(INSTALL) -d $(TARGET_SHARE_DIR)/minisatip
 	$(INSTALL_COPY) $(PKG_BUILD_DIR)/html $(TARGET_SHARE_DIR)/minisatip
