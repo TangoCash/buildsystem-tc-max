fcc2f102bfb0c08194fbe71bd789da67c5f36dd1
max_10 <max_10@gmx.de>
2022-05-14 20:56:55 +0200

    - libfuse: Define closefrom if not available fix build with glibc 2.34+

--- package/target/libfuse/libfuse.mk
+++ package/target/libfuse/libfuse.mk
@@ -7,6 +7,8 @@ LIBFUSE_SOURCE  = fuse-$(LIBFUSE_VERSION).tar.gz
 LIBFUSE_SITE    = https://github.com/libfuse/libfuse/releases/download/fuse-$(LIBFUSE_VERSION)
 LIBFUSE_DEPENDS = bootstrap
 
+LIBFUSE_AUTORECONF = YES
+
 LIBFUSE_CONF_OPTS = \
 	--disable-static \
 	--disable-example \
