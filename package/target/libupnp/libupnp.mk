4dcd4abfab8ab5a76fb724316ffdde6457e629f5
max_10 <max_10@gmx.de>
2022-05-14 20:52:20 +0200

    - libupnp: backported patch to fix build with glibc 2.34+

--- package/target/libupnp/libupnp.mk
+++ package/target/libupnp/libupnp.mk
@@ -1,12 +1,20 @@
+#
+# libupnp
+#
 LIBUPNP_VERSION = 1.6.25
 LIBUPNP_DIR     = libupnp-$(LIBUPNP_VERSION)
 LIBUPNP_SOURCE  = libupnp-$(LIBUPNP_VERSION).tar.bz2
 LIBUPNP_SITE    = http://sourceforge.net/projects/pupnp/files/pupnp/libUPnP%20$(LIBUPNP_VERSION)
 LIBUPNP_DEPENDS = bootstrap
 
+LIBUPNP_CONF_ENV = \
+	ac_cv_lib_compat_ftime=no
+
 LIBUPNP_CONF_OPTS = \
 	--enable-shared \
-	--disable-static
+	--disable-static \
+	--disable-samples \
+	--enable-reuseaddr
 
 $(D)/libupnp:
 	$(START_BUILD)
