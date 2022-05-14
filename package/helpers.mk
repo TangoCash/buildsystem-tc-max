efeaaacb32aba433478d36d055b8b3dfc9515e2e
max_10 <max_10@gmx.de>
2022-05-14 21:05:06 +0200

    - helpers: fix add svn support

--- package/helpers.mk
+++ package/helpers.mk
@@ -50,6 +50,13 @@ define EXTRACT
 	      $(CD) $(1)/$($(PKG)_DIR); git checkout -q $($(PKG)_CHECKOUT); \
 	    fi; \
 	    ;; \
+	  *.svn | svn.*) \
+	    cp -a -t $(1) $(DL_DIR)/$($(PKG)_SOURCE); \
+	    if test $($(PKG)_CHECKOUT); then \
+	      $(call MESSAGE,"svn checkout $($(PKG)_CHECKOUT)"); \
+	      $(CD) $(1)/$($(PKG)_DIR); svn checkout $($(PKG)_CHECKOUT); \
+	    fi; \
+	    ;; \
 	  *) \
 	    $(call MESSAGE,"Cannot extract $($(PKG)_SOURCE)"); \
 	    false ;; \
