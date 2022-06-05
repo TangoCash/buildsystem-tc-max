700841807286f0f6745b130664aa46cf2dae3c73
max_10 <max_10@gmx.de>
2022-06-05 21:03:52 +0200

    - luacurl: update build

--- package/target/luacurl/luacurl.mk
+++ package/target/luacurl/luacurl.mk
@@ -1,17 +1,25 @@
+################################################################################
 #
 # luacurl
 #
+################################################################################
+
 LUACURL_VERSION = git
 LUACURL_DIR     = lua-curlv3.git
 LUACURL_SOURCE  = lua-curlv3.git
 LUACURL_SITE    = https://github.com/Lua-cURL
 LUACURL_DEPENDS = bootstrap libcurl lua
 
+LUACURL_MAKE_OPTS = \
+	LUA_CMOD=$(TARGET_LIB_DIR)/lua/$(LUA_ABIVERSION) \
+	LUA_LMOD=$(TARGET_SHARE_DIR)/lua/$(LUA_ABIVERSION) \
+	LIBDIR=$(TARGET_LIB_DIR) \
+	LUA_INC=$(TARGET_INCLUDE_DIR) \
+	CURL_LIBS="-L$(TARGET_LIB_DIR) -lcurl"
+
 $(D)/luacurl:
 	$(call PREPARE)
 	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE) CC=$(TARGET_CC) LDFLAGS="-L$(TARGET_LIB_DIR)" \
-			LIBDIR=$(TARGET_LIB_DIR) \
-			LUA_INC=$(TARGET_INCLUDE_DIR); \
-		$(MAKE) install LUA_CMOD=$(TARGET_LIB_DIR)/lua/$(LUA_ABIVERSION) LUA_LMOD=$(TARGET_SHARE_DIR)/lua/$(LUA_ABIVERSION)
+		$(TARGET_CONFIGURE_ENV) $(MAKE) $(LUACURL_MAKE_OPTS); \
+		$(TARGET_CONFIGURE_ENV) $(MAKE)  $(LUACURL_MAKE_OPTS) install
 	$(call TARGET_FOLLOWUP)
