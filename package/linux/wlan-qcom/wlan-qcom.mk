c53194bc05ec32c2be21392b8207d29c4aeecd7f
max_10 <max_10@gmx.de>
2022-06-20 19:33:34 +0200

    - wlan-qcom: update build

--- package/linux/wlan-qcom/wlan-qcom.mk
+++ package/linux/wlan-qcom/wlan-qcom.mk
@@ -10,14 +10,22 @@ WLAN_QCOM_SOURCE  = qcacld-2.0-$(WLAN_QCOM_VERSION).tar.gz
 WLAN_QCOM_SITE    = https://www.codeaurora.org/cgit/external/wlan/qcacld-2.0/snapshot
 WLAN_QCOM_DEPENDS = bootstrap kernel wlan-qcom-firmware wireless-regdb
 
-$(D)/wlan-qcom:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE) $(KERNEL_MAKE_VARS); \
-	$(INSTALL_DATA) wlan.ko $(TARGET_MODULES_DIR)/extra
+define WLAN_QCOM_INSTALL_BINARY
+	$(INSTALL_DATA) wlan.ko $(TARGET_MODULES_DIR)/extra/wlan.ko
 	mkdir -p ${TARGET_DIR}/etc/modules-load.d
 	for i in wlan; do \
 		echo $$i >> ${TARGET_DIR}/etc/modules-load.d/wlan.conf; \
 	done
+endef
+WLAN_QCOM_PRE_INSTALL_TARGET_HOOKS += WLAN_QCOM_INSTALL_BINARY
+
+define WLAN_QCOM_RUN_DEPMOD
 	$(LINUX_RUN_DEPMOD)
+endef
+WLAN_QCOM_POST_INSTALL_TARGET_HOOKS += WLAN_QCOM_RUN_DEPMOD
+
+$(D)/wlan-qcom:
+	$(call PREPARE)
+	$(CHDIR)/$($(PKG)_DIR); \
+		$(MAKE) $(KERNEL_MAKE_VARS)
 	$(call TARGET_FOLLOWUP)
