48d385f71a7e39e8000f07a66cdf9e1b9ce184ca
max_10 <max_10@gmx.de>
2022-07-04 00:26:37 +0200

    - libdpf: update build

--- package/target/libdpf/libdpf.mk
+++ package/target/libdpf/libdpf.mk
@@ -10,16 +10,19 @@ LIBDPF_SOURCE  = dpf-ax.git
 LIBDPF_SITE    = $(MAX-GIT-GITHUB)
 LIBDPF_DEPENDS = bootstrap libusb-compat
 
-define LIBDPF_INSTALL_FILES
+LIBDPF_MAKE_OPTS = \
+	CC=$(TARGET_CC) PREFIX=$(TARGET_DIR)/usr
+
+define LIBDPF_INSTALL
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/dpflib/libdpf.a $(TARGET_LIB_DIR)/libdpf.a
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/dpflib/dpf.h $(TARGET_INCLUDE_DIR)/libdpf/libdpf.h
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/include/spiflash.h $(TARGET_INCLUDE_DIR)/libdpf/spiflash.h
 	$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/include/usbuser.h $(TARGET_INCLUDE_DIR)/libdpf/usbuser.h
 endef
-LIBDPF_POST_FOLLOWUP_HOOKS += LIBDPF_INSTALL_FILES
+LIBDPF_POST_FOLLOWUP_HOOKS += LIBDPF_INSTALL
 
 $(D)/libdpf:
 	$(call PREPARE)
 	$(CHDIR)/$($(PKG)_DIR); \
-		make -C dpflib libdpf.a CC=$(TARGET_CC) PREFIX=$(TARGET_DIR)/usr
+		make -C dpflib libdpf.a $($(PKG)_MAKE_OPTS)
 	$(call TARGET_FOLLOWUP)
