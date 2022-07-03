429ac47430785248ab64937b18122a0045f3f544
max_10 <max_10@gmx.de>
2022-07-04 00:27:42 +0200

    - minisatip: update build

--- package/target/minisatip/minisatip.mk
+++ package/target/minisatip/minisatip.mk
@@ -17,23 +17,21 @@ MINISATIP_CONF_OPTS = \
 	--enable-static \
 	--disable-netcv
 
+MINISATIP_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
 define MINISATIP_INSTALL_INIT_SYSV
 	$(INSTALL_DATA) $(PKG_FILES_DIR)/minisatip $(TARGET_DIR)/etc/default/minisatip
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/minisatip.init $(TARGET_DIR)/etc/init.d/minisatip
 	$(UPDATE-RC.D) minisatip defaults 75 25
 endef
 
-define MINISATIP_INSTALL_FILES
+define MINISATIP_INSTALL
 	$(INSTALL_EXEC) -D $(PKG_BUILD_DIR)/minisatip $(TARGET_BIN_DIR)
 	$(INSTALL) -d $(TARGET_SHARE_DIR)/minisatip
 	$(INSTALL_COPY) $(PKG_BUILD_DIR)/html $(TARGET_SHARE_DIR)/minisatip
 endef
-MINISATIP_POST_FOLLOWUP_HOOKS += MINISATIP_INSTALL_FILES
+MINISATIP_POST_FOLLOWUP_HOOKS += MINISATIP_INSTALL
 
 $(D)/minisatip:
-	$(call PREPARE)
-	$(call TARGET_CONFIGURE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE)
-	$(call TARGET_FOLLOWUP)
+	$(call autotools-package,$(PKG_NO_INSTALL))
