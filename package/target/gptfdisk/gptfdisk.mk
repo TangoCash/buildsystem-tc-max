8540f89945edf1040629fc8d6f45046d35f1c082
max_10 <max_10@gmx.de>
2022-07-04 00:23:57 +0200

    - gptfdisk: update build

--- package/target/gptfdisk/gptfdisk.mk
+++ package/target/gptfdisk/gptfdisk.mk
@@ -10,10 +10,18 @@ GPTFDISK_SOURCE  = gptfdisk-$(GPTFDISK_VERSION).tar.gz
 GPTFDISK_SITE    = https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$(GPTFDISK_VERSION)
 GPTFDISK_DEPENDS = bootstrap e2fsprogs ncurses popt
 
+GPTFDISK_SBINARIES = sgdisk
+
+GPTFDISK_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
+GPTFDISK_MAKE_OPTS = \
+	$(GPTFDISK_SBINARIES)
+
+define GPTFDISK_INSTALL_BINARY
+	$(INSTALL_EXEC) $(PKG_BUILD_DIR)/sgdisk $(TARGET_SBIN_DIR)/sgdisk
+endef
+GPTFDISK_POST_FOLLOWUP_HOOKS += GPTFDISK_INSTALL_BINARY
+
 $(D)/gptfdisk:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE) sgdisk; \
-		$(INSTALL_EXEC) sgdisk $(TARGET_SBIN_DIR)/sgdisk
-	$(call TARGET_FOLLOWUP)
+	$(call make-package,$(PKG_NO_INSTALL))
