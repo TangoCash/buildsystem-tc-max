ec94f5847b94bc0b1a83a69b281dd4a852e3b367
max_10 <max_10@gmx.de>
2022-07-04 00:30:07 +0200

    - zlib: update build

--- package/target/zlib/zlib.mk
+++ package/target/zlib/zlib.mk
@@ -10,20 +10,16 @@ ZLIB_SOURCE  = zlib-$(ZLIB_VERSION).tar.xz
 ZLIB_SITE    = https://sourceforge.net/projects/libpng/files/zlib/$(ZLIB_VERSION)
 ZLIB_DEPENDS = bootstrap
 
+ZLIB_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV) \
+	mandir=$(REMOVE_mandir) \
+	./configure \
+	--prefix=/usr \
+	--shared \
+	--uname=Linux; \
+
 ZLIB_CONF_ENV = \
 	mandir=$(REMOVE_mandir)
 
-ZLIB_CONF_OPTS = \
-	--prefix=/usr \
-	--shared \
-	--uname=Linux
-
 $(D)/zlib:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		./configure $($(PKG)_CONF_OPTS); \
-		$(MAKE); \
-		ln -sf /bin/true ldconfig; \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(call TARGET_FOLLOWUP)
+	$(call make-package)
