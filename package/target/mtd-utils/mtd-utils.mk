a080f4a6ca8f0f5646f186088f3397799595f8a9
max_10 <max_10@gmx.de>
2022-07-07 14:09:03 +0200

    - mtd-utils: rework build

--- package/target/mtd-utils/mtd-utils.mk
+++ package/target/mtd-utils/mtd-utils.mk
@@ -4,18 +4,18 @@
 #
 ################################################################################
 
-MTD_UTILS_VERSION = 1.5.2
+MTD_UTILS_VERSION = 2.1.2
 MTD_UTILS_DIR = mtd-utils-$(MTD_UTILS_VERSION)
 MTD_UTILS_SOURCE = mtd-utils-$(MTD_UTILS_VERSION).tar.bz2
 MTD_UTILS_SITE = https://infraroot.at/pub/mtd
 
-MTD_UTILS_DEPENDS = bootstrap zlib lzo e2fsprogs
+TD_UTILS_DEPENDS = bootstrap zlib lzo e2fsprogs
+
+MTD_UTILS_CONF_OPTS = \
+	--disable-tests \
+	--without-zstd \
+	--without-ubifs \
+	--without-xattr
 
 $(D)/mtd-utils:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) \
-		$(MAKE) PREFIX= CC=$(TARGET_CC) LD=$(TARGET_LD) STRIP=$(TARGET_STRIP) WITHOUT_XATTR=1 DESTDIR=$(TARGET_DIR); \
-		cp -a $(BUILD_DIR)/mtd-utils-$(MTD_UTILS_VERSION)/mkfs.jffs2 $(TARGET_SBIN_DIR)
-		cp -a $(BUILD_DIR)/mtd-utils-$(MTD_UTILS_VERSION)/sumtool $(TARGET_SBIN_DIR)
-	$(call TARGET_FOLLOWUP)
+	$(call autotools-package)
