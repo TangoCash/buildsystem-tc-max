599c912a67af9088831da47dafc03eb80499d450
max_10 <max_10@gmx.de>
2022-07-07 14:04:54 +0200

    - lua-curl: rework build

--- package/target/lua-curl/lua-curl.mk
+++ package/target/lua-curl/lua-curl.mk
@@ -11,16 +11,15 @@ LUA_CURL_SITE = https://github.com/Lua-cURL
 
 LUA_CURL_DEPENDS = bootstrap libcurl lua
 
+LUA_CURL_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
 LUA_CURL_MAKE_OPTS = \
-	LUA_CMOD=$(TARGET_LIB_DIR)/lua/$(LUA_ABIVERSION) \
-	LUA_LMOD=$(TARGET_SHARE_DIR)/lua/$(LUA_ABIVERSION) \
 	LIBDIR=$(TARGET_LIB_DIR) \
 	LUA_INC=$(TARGET_INCLUDE_DIR) \
+	LUA_CMOD=$(libdir)/lua/$(LUA_ABIVERSION) \
+	LUA_LMOD=$(datadir)/lua/$(LUA_ABIVERSION) \
 	CURL_LIBS="-L$(TARGET_LIB_DIR) -lcurl"
 
 $(D)/lua-curl:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) $(MAKE) $(LUA_CURL_MAKE_OPTS); \
-		$(TARGET_CONFIGURE_ENV) $(MAKE) $(LUA_CURL_MAKE_OPTS) install
-	$(call TARGET_FOLLOWUP)
+	$(call make-package)
