57394b2ccefd96c4604d13afe329bf4e4ba86696
max_10 <max_10@gmx.de>
2022-07-07 14:15:50 +0200

    - smartmontools: rework build

--- package/target/smartmontools/smartmontools.mk
+++ package/target/smartmontools/smartmontools.mk
@@ -11,10 +11,20 @@ SMARTMONTOOLS_SITE = https://sourceforge.net/projects/smartmontools/files/smartm
 
 SMARTMONTOOLS_DEPENDS = bootstrap
 
+SMARTMONTOOLS_CONF_OPTS = \
+	--docdir=$(REMOVE_docdir) \
+	--with-drivedbdir=no \
+	--with-initscriptdir=no \
+	--with-smartdplugindir=no \
+	--with-smartdscriptdir=$(REMOVE_sysconfdir) \
+	--with-update-smart-drivedb=no \
+	--without-gnupg
+
+define SMARTMONTOOLS_TARGET_CLEANUP
+	rm -f $(TARGET_DIR)/etc/smartd.conf
+	rm -f $(TARGET_SBIN_DIR)/smartd
+endef
+SMARTMONTOOLS_TARGET_CLEANUP_HOOKS += SMARTMONTOOLS_TARGET_CLEANUP
+
 $(D)/smartmontools:
-	$(call PREPARE)
-	$(call TARGET_CONFIGURE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE); \
-		$(INSTALL_EXEC) smartctl $(TARGET_SBIN_DIR)/smartctl
-	$(call TARGET_FOLLOWUP)
+	$(call autotools-package)
