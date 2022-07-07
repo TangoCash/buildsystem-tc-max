6dbfc6330ac86a12637b227a2fc022501922ecb0
max_10 <max_10@gmx.de>
2022-07-07 14:04:34 +0200

    - lua: rework build

--- package/target/lua/lua.mk
+++ package/target/lua/lua.mk
@@ -12,23 +12,25 @@ LUA_SITE = https://www.lua.org/ftp
 
 LUA_DEPENDS = bootstrap host-lua ncurses
 
+LUA_MAKE_ARGS = \
+	linux
+
+LUA_MAKE_OPTS = \
+	BUILDMODE=dynamic \
+	PKG_VERSION=$(LUA_VERSION) \
+	CC=$(TARGET_CC) \
+	CPPFLAGS="$(TARGET_CPPFLAGS) -fPIC" \
+	LDFLAGS="-L$(TARGET_LIB_DIR)" \
+	AR="$(TARGET_AR) rcu"
+
+LUA_MAKE_INSTALL_OPTS = \
+	INSTALL_TOP=$(TARGET_DIR)/usr \
+	INSTALL_MAN=$(TARGET_DIR)$(REMOVE_mandir)
+
 define LUA_TARGET_CLEANUP
 	rm -f $(addprefix $(TARGET_BIN_DIR)/,luac)
 endef
 LUA_TARGET_CLEANUP_HOOKS += LUA_TARGET_CLEANUP
 
 $(D)/lua:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE) linux \
-			BUILDMODE=dynamic \
-			PKG_VERSION=$(LUA_VERSION) \
-			CC=$(TARGET_CC) \
-			CPPFLAGS="$(TARGET_CPPFLAGS) -fPIC" \
-			LDFLAGS="-L$(TARGET_LIB_DIR)" \
-			AR="$(TARGET_AR) rcu"; \
-		$(MAKE) install \
-			INSTALL_TOP=$(TARGET_DIR)/usr \
-			INSTALL_MAN=$(TARGET_DIR)$(REMOVE_mandir); \
-		$(INSTALL_DATA) -D $(PKG_BUILD_DIR)/lua.pc $(PKG_CONFIG_PATH)/lua.pc
-	$(call TARGET_FOLLOWUP)
+	$(call make-package)
