74cd9e7fb0580fa5151cab0a8258ccd5066d158a
max_10 <max_10@gmx.de>
2022-07-07 14:13:38 +0200

    - rtmpdump: rework build

--- package/target/rtmpdump/rtmpdump.mk
+++ package/target/rtmpdump/rtmpdump.mk
@@ -11,14 +11,19 @@ RTMPDUMP_SITE = https://github.com/oe-alliance
 
 RTMPDUMP_DEPENDS = bootstrap zlib openssl
 
+RTMPDUMP_MAKE_ENV = \
+	CROSS_COMPILE=$(TARGET_CROSS) \
+	XCFLAGS="-I$(TARGET_INCLUDE_DIR) -L$(TARGET_LIB_DIR)" \
+	LDFLAGS="-L$(TARGET_LIB_DIR)"
+
+RTMPDUMP_MAKE_OPTS = \
+	prefix=/usr \
+	MANDIR=$(TARGET_DIR)$(REMOVE_mandir)
+
 define RTMPDUMP_TARGET_CLEANUP
 	rm -f $(addprefix $(TARGET_SBIN_DIR)/,rtmpgw rtmpsrv rtmpsuck)
 endef
 RTMPDUMP_TARGET_CLEANUP_HOOKS += RTMPDUMP_TARGET_CLEANUP
 
 $(D)/rtmpdump:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) XCFLAGS="-I$(TARGET_INCLUDE_DIR) -L$(TARGET_LIB_DIR)" LDFLAGS="-L$(TARGET_LIB_DIR)"; \
-		$(MAKE) install prefix=/usr DESTDIR=$(TARGET_DIR) MANDIR=$(TARGET_DIR)$(REMOVE_mandir)
-	$(call TARGET_FOLLOWUP)
+	$(call make-package)
