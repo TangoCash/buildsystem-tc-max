ad789bb411c300bbf0332926466763f6947e09f1
max_10 <max_10@gmx.de>
2022-07-07 13:54:04 +0200

    - pkg-utils.mk: update

--- package/pkg-utils.mk
+++ package/pkg-utils.mk
@@ -1,11 +1,123 @@
 ################################################################################
 #
-# makefile to keep buildsystem helpers
+# This file contains various utility functions used by the package
+# infrastructure, or by the packages themselves.
 #
 ################################################################################
 
+pkgname = $(subst -config,,$(subst -upgradeconfig,,$(basename $(@F))))
+pkg = $(call LOWERCASE,$(pkgname))
+PKG = $(call UPPERCASE,$(pkgname))
+
+PKG_BUILD_DIR = $(BUILD_DIR)/$($(PKG)_DIR)
+PKG_FILES_DIR = $(BASE_DIR)/package/*/$(pkgname)/files
+PKG_PATCHES_DIR = $(BASE_DIR)/package/*/$(pkgname)/patches
+
+PKG_HOST_PACKAGE = $(if $(filter $(firstword $(subst -, ,$(pkg))),host),YES,NO)
+PKG_TARGET_PACKAGE = $(if $(filter $(PKG_HOST_PACKAGE),NO),YES,NO)
+
+# -----------------------------------------------------------------------------
+
+# check for necessary $(PKG) variables
+define PKG_CHECK_VARIABLES
+
+# patch
+ifndef $(PKG)_PATCH_DIR
+  $(PKG)_PATCH_DIR = $(PKG_PATCHES_DIR)
+endif
+ifndef $(PKG)_PATCH_CUSTOM
+  $(PKG)_PATCH_CUSTOM = $($(PKG)_PATCH)
+endif
+
+# autoreconf
+ifndef $(PKG)_AUTORECONF
+  $(PKG)_AUTORECONF = NO
+endif
+ifndef $(PKG)_AUTORECONF_CMD
+  $(PKG)_AUTORECONF_CMD = autoreconf -fi -I $(TARGET_SHARE_DIR)/aclocal
+endif
+ifndef $(PKG)_AUTORECONF_ENV
+  $(PKG)_AUTORECONF_ENV =
+endif
+ifndef $(PKG)_AUTORECONF_OPTS
+  $(PKG)_AUTORECONF_OPTS =
+endif
+
+# cmake / configure / meson
+ifndef $(PKG)_CMAKE
+  $(PKG)_CMAKE = cmake
+endif
+ifndef $(PKG)_CONFIGURE_CMD
+  $(PKG)_CONFIGURE_CMD = configure
+endif
+ifndef $(PKG)_CONFIGURE_CMDS
+  ifeq ($(PKG_HOST_PACKAGE),YES)
+    $(PKG)_CONFIGURE_CMDS = $$(HOST_CONFIGURE_CMDS)
+  else
+    $(PKG)_CONFIGURE_CMDS = $$(TARGET_CONFIGURE_CMDS)
+  endif
+endif
+ifndef $(PKG)_CONF_ENV
+  $(PKG)_CONF_ENV =
+endif
+ifndef $(PKG)_CONF_OPTS
+  $(PKG)_CONF_OPTS =
+endif
+
+# make
+ifndef $(PKG)_MAKE
+  $(PKG)_MAKE = $(MAKE)
+endif
+ifndef $(PKG)_MAKE_ENV
+  $(PKG)_MAKE_ENV =
+endif
+ifndef $(PKG)_MAKE_ARGS
+  $(PKG)_MAKE_ARGS =
+endif
+ifndef $(PKG)_MAKE_OPTS
+  $(PKG)_MAKE_OPTS =
+endif
+
+# make install
+ifndef $(PKG)_MAKE_INSTALL
+  $(PKG)_MAKE_INSTALL = $($(PKG)_MAKE)
+endif
+ifndef $(PKG)_MAKE_INSTALL_ENV
+  $(PKG)_MAKE_INSTALL_ENV = $($(PKG)_MAKE_ENV)
+endif
+ifndef $(PKG)_MAKE_INSTALL_ARGS
+  $(PKG)_MAKE_INSTALL_ARGS = install
+endif
+ifndef $(PKG)_MAKE_INSTALL_OPTS
+  $(PKG)_MAKE_INSTALL_OPTS = $($(PKG)_MAKE_OPTS)
+endif
+
+# ninja
+ifndef $(PKG)_NINJA_ENV
+  $(PKG)_NINJA_ENV =
+endif
+ifndef $(PKG)_NINJA_OPTS
+  $(PKG)_NINJA_OPTS =
+endif
+
+endef # PKG_CHECK_VARIABLES
+
+pkg-check-variables = $(call PKG_CHECK_VARIABLES)
+
+# -----------------------------------------------------------------------------
+
+# PKG "control-flag" variables
+PKG_NO_EXTRACT = pkg-no-extract
+PKG_NO_PATCHES = pkg-no-patches
+PKG_NO_BUILD = pkg-no-build
+PKG_NO_INSTALL = pkg-no-install
+
+# -----------------------------------------------------------------------------
+
+#
 # Manipulation of .config files based on the Kconfig infrastructure.
 # Used by the BusyBox package, the Linux kernel package, and more.
+#
 
 define KCONFIG_ENABLE_OPT # (option, file)
 	$(SED) "/\\<$(1)\\>/d" $(2)
@@ -21,83 +133,3 @@ define KCONFIG_DISABLE_OPT # (option, file)
 	$(SED) "/\\<$(1)\\>/d" $(2)
 	echo '# $(1) is not set' >> $(2)
 endef
-
-# -----------------------------------------------------------------------------
-
-# BS Revision
-BS_REV=$(shell cd $(BASE_DIR); git log | grep "^commit" | wc -l)
-# Neutrino mp Revision
-NMP_REV=$(shell cd $(SOURCE_DIR)/$(NEUTRINO_DIR); git log | grep "^commit" | wc -l)
-# libstb-hal Revision
-HAL_REV=$(shell cd $(SOURCE_DIR)/$(LIBSTB_HAL_DIR); git log | grep "^commit" | wc -l)
-
-# -----------------------------------------------------------------------------
-
-rewrite-test:
-	@printf "$(TERM_YELLOW)---> create rewrite-dependency_libs.txt ... "
-	$(shell cd $(TARGET_DIR)/usr/lib && grep ^dependency_libs *.la > $(BUILD_DIR)/rewrite-dependency_libs.txt)
-	@printf "done\n$(TERM_NORMAL)"
-	@printf "$(TERM_YELLOW)---> create rewrite-libdir.txt ... "
-	$(shell cd $(TARGET_DIR)/usr/lib && grep ^libdir *.la > $(BUILD_DIR)/rewrite-libdir.txt)
-	@printf "done\n$(TERM_NORMAL)"
-	@printf "$(TERM_YELLOW)---> create rewrite-pkgconfig.txt ... "
-	$(shell cd $(TARGET_DIR)/usr/lib/pkgconfig && grep ^prefix * > $(BUILD_DIR)/rewrite-pkgconfig.txt)
-	@printf "done\n$(TERM_NORMAL)"
-
-patch:
-	@make neutrino-patch
-	@make libstb-hal-patch
-	@make neutrino-plugins-patch
-
-neutrino-patch:
-	@printf "$(TERM_YELLOW)---> create $(NEUTRINO)-$(DATE).patch ... $(TERM_NORMAL)"
-	$(shell cd $(SOURCE_DIR)/$(NEUTRINO_DIR) && git diff > $(BUILD_DIR)/$(NEUTRINO)-$(DATE).patch)
-	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
-
-libstb-hal-patch:
-	@printf "$(TERM_YELLOW)---> create $(LIBSTB_HAL)-$(DATE).patch ... $(TERM_NORMAL)"
-	$(shell cd $(SOURCE_DIR)/$(LIBSTB_HAL_DIR) && git diff > $(BUILD_DIR)/$(LIBSTB_HAL)-$(DATE).patch)
-	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
-
-neutrino-plugins-patch:
-	@printf "$(TERM_YELLOW)---> create $(NEUTRINO_PLUGINS_DIR)-$(DATE).patch ... $(TERM_NORMAL)"
-	$(shell cd $(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR) && git diff > $(BUILD_DIR)/$(NEUTRINO_PLUGINS_DIR)-$(DATE).patch)
-	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
-
-# -----------------------------------------------------------------------------
-
-TUXBOX_CUSTOMIZE = [ -x support/scripts/$(notdir $@)-local.sh ] && \
-	support/scripts/$(notdir $@)-local.sh \
-	$(RELEASE_DIR) \
-	$(TARGET_DIR) \
-	$(BASE_DIR) \
-	$(SOURCE_DIR) \
-	$(IMAGE_DIR) \
-	$(BOXMODEL) \
-	$(FLAVOUR) \
-	$(DATE) \
-	|| true
-
-# -----------------------------------------------------------------------------
-
-ifeq ($(GITSSH),1)
-MAX-GIT-GITHUB = git@github.com:MaxWiesel
-URL_1          = https://github.com/MaxWiesel
-URL_2          = $(MAX-GIT-GITHUB)
-else
-MAX-GIT-GITHUB = https://github.com/MaxWiesel
-URL_1          = git@github.com:MaxWiesel
-URL_2          = $(MAX-GIT-GITHUB)
-endif
-
-REPOSITORIES = \
-	. \
-	$(DL_DIR)/libstb-hal-max.git \
-	$(DL_DIR)/neutrino-mp-max.git \
-	$(DL_DIR)/neutrino-plugins.git \
-	$(DL_DIR)/ofgwrite-nmp.git
-
-switch-url:
-	for repo in $(REPOSITORIES); do \
-		$(SED) 's|url = $(URL_1)|url = $(URL_2)|' $$repo/.git/config; \
-	done
