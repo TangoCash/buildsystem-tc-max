2082d4de5179fa1e909869caacc3dbfd3c68b28c
max_10 <max_10@gmx.de>
2022-07-07 14:17:15 +0200

    - usb-modeswitch: rework build

--- package/target/usb-modeswitch/usb-modeswitch.mk
+++ package/target/usb-modeswitch/usb-modeswitch.mk
@@ -11,9 +11,11 @@ USB_MODESWITCH_SITE = http://www.draisberghof.de/usb_modeswitch
 
 USB_MODESWITCH_DEPENDS = bootstrap libusb usb-modeswitch-data
 
+USB_MODESWITCH_MAKE_ENV = \
+	$(TARGET_CONFIGURE_ENV)
+
+USB_MODESWITCH_MAKE_INSTALL_OPTS = \
+	MANDIR=$(TARGET_DIR)$(REMOVE_mandir)
+
 $(D)/usb-modeswitch:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(TARGET_CONFIGURE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR) MANDIR=$(TARGET_DIR)$(REMOVE_mandir)
-	$(call TARGET_FOLLOWUP)
+	$(call make-package)
