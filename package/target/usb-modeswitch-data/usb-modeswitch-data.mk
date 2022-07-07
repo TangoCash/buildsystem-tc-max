1eccc195718d90fbf354c5d81a23308db5219b63
max_10 <max_10@gmx.de>
2022-07-07 14:17:36 +0200

    - usb-modeswitch-data: rework build

--- package/target/usb-modeswitch-data/usb-modeswitch-data.mk
+++ package/target/usb-modeswitch-data/usb-modeswitch-data.mk
@@ -12,7 +12,4 @@ USB_MODESWITCH_DATA_SITE = http://www.draisberghof.de/usb_modeswitch
 USB_MODESWITCH_DATA_DEPENDS = bootstrap
 
 $(D)/usb-modeswitch-data:
-	$(call PREPARE)
-	$(CHDIR)/$($(PKG)_DIR); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(call TARGET_FOLLOWUP)
+	$(call make-package)
