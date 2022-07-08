58651e1935bda62ba1ec7654aaaf55c6f33d531c
max_10 <max_10@gmx.de>
2022-07-08 16:57:37 +0200

    - slingshot: rework build

--- package/target/slingshot/slingshot.mk
+++ package/target/slingshot/slingshot.mk
@@ -9,7 +9,5 @@ SLINGSHOT_DIR = slingshot-$(SLINGSHOT_VERSION)
 SLINGSHOT_SOURCE = slingshot-$(SLINGSHOT_VERSION).tar.gz
 SLINGSHOT_SITE = $(call github,gvvaughan,slingshot,v$(SLINGSHOT_VERSION))
 
-SLINGSHOT_DEPENDS = bootstrap
-
 $(D)/slingshot:
-	$(call individual-package,$(PKG_NO_EXTRACT) $(PKG_NO_PATCHES) $(PKG_NO_BUILD) $(PKG_NO_INSTALL))
+	$(call DOWNLOAD,$($(PKG)_SOURCE))
