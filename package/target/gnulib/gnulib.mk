bf79d7482070fbc7f5a35062a03158663bfd7f23
max_10 <max_10@gmx.de>
2022-07-08 16:57:16 +0200

    - gnulib: rework build

--- package/target/gnulib/gnulib.mk
+++ package/target/gnulib/gnulib.mk
@@ -7,12 +7,7 @@
 GNULIB_VERSION = 20140202
 GNULIB_DIR = gnulib-$(GNULIB_VERSION)-stable
 GNULIB_SOURCE = gnulib-$(GNULIB_VERSION)-stable.tar.gz
-
 GNULIB_SITE = http://erislabs.net/ianb/projects/gnulib
 
-GNULIB_DEPENDS = bootstrap
-
 $(D)/gnulib:
-	$(call STARTUP)
 	$(call DOWNLOAD,$($(PKG)_SOURCE))
-	$(TOUCH)
