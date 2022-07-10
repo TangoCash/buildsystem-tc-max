b022158a587762b13ab43e82c3721e65bc40846b
max_10 <max_10@gmx.de>
2022-07-10 16:37:31 +0200

    - tzdata: rework build

--- package/target/tzdata/tzdata.mk
+++ package/target/tzdata/tzdata.mk
@@ -5,35 +5,27 @@
 ################################################################################
 
 TZDATA_VERSION = 2022a
-TZDATA_DIR = timezone
+TZDATA_DIR = tzdata$(TZDATA_VERSION)
 TZDATA_SOURCE = tzdata$(TZDATA_VERSION).tar.gz
 TZDATA_SITE = https://data.iana.org/time-zones/releases
 
-TZDATA_DEPENDS = bootstrap host-zic
+TZDATA_DEPENDS = host-zic
+
+# fix non-existing subdir in tzdata tarball
+TZDATA_EXTRACT_DIR = $($(PKG)_DIR)
 
 TZDATA_ZONELIST = \
-	africa antarctica asia australasia \
-	europe northamerica southamerica \
-	factory etcetera backward
+	africa antarctica asia australasia europe northamerica \
+	southamerica etcetera backward factory
 
-TZDATA_LOCALTIME = CET
-
-define TZDATA_INSTALL_FILES
-	$(INSTALL_DATA) $(PKG_FILES_DIR)/timezone.xml $(TARGET_DIR)/etc/
-	ln -sf /usr/share/zoneinfo/$(TZDATA_LOCALTIME) $(TARGET_DIR)/etc/localtime
-endef
-TZDATA_POST_FOLLOWUP_HOOKS += TZDATA_INSTALL_FILES
-
-$(D)/tzdata:
-	$(call STARTUP)
-	$(call CLEANUP)
-	$(call DOWNLOAD,$($(PKG)_SOURCE))
-	$(MKDIR)/$($(PKG)_DIR)
-	$(call EXTRACT,$(PKG_BUILD_DIR))
-	$(call APPLY_PATCHES,$(PKG_PATCHES_DIR))
+define TZDATA_BUILD_CMDS
 	$(CHDIR)/$($(PKG)_DIR); \
 		unset ${!LC_*}; LANG=POSIX; LC_ALL=POSIX; export LANG LC_ALL; \
-		$(HOST_DIR)/bin/zic -b fat -d zoneinfo.tmp $(TZDATA_ZONELIST); \
+		$(HOST_ZIC_BINARY) -b fat -d zoneinfo.tmp $(TZDATA_ZONELIST)
+endef
+
+define TZDATA_INSTALL_CMDS
+	$(CHDIR)/$($(PKG)_DIR); \
 		sed -n '/zone=/{s/.*zone="\(.*\)".*$$/\1/; p}' $(PKG_FILES_DIR)/timezone.xml | sort -u | \
 		while read x; do \
 			find zoneinfo.tmp -type f -name $$x | sort | \
@@ -41,4 +33,15 @@ $(D)/tzdata:
 				test -e $$y && $(INSTALL_DATA) -D $$y $(TARGET_SHARE_DIR)/zoneinfo/$$x; \
 			done; \
 		done
-	$(call TARGET_FOLLOWUP)
+endef
+
+TZDATA_LOCALTIME = CET
+
+define TZDATA_INSTALL_TIMEZONE_FILES
+	$(INSTALL_DATA) -D $(PKG_FILES_DIR)/timezone.xml $(TARGET_DIR)/etc/timezone.xml
+	ln -sf /usr/share/zoneinfo/$(TZDATA_LOCALTIME) $(TARGET_DIR)/etc/localtime
+endef
+TZDATA_POST_INSTALL_HOOKS += TZDATA_INSTALL_TIMEZONE_FILES
+
+$(D)/tzdata: | bootstrap
+	$(call generic-package)
