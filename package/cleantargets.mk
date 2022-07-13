9763354da05e4dbad0f446d075b6a9a7f01215e3
max_10 <max_10@gmx.de>
2022-07-13 15:44:28 +0200

    - readd DEPS_DIR

--- package/cleantargets.mk
+++ package/cleantargets.mk
@@ -6,20 +6,20 @@
 
 %-clean:
 	@printf "$(TERM_YELLOW)===> clean $(subst -clean,,$@) .. $(TERM_NORMAL)"
-	$(shell find $(D) -name $(subst -clean,,$@) -delete)
+	$(shell find $(DEPS_DIR) -name $(subst -clean,,$@) -delete)
 	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 depsclean:
-	$(shell find $(D) ! -name "*\.*" -delete )
+	$(shell find $(DEPS_DIR) ! -name "*\.*" -delete )
 
 clean: depsclean
 	@printf "$(TERM_YELLOW)===> cleaning system build directories and files .. $(TERM_NORMAL)"
 	@-rm -rf $(HOST_DIR)
 	@-rm -rf $(BASE_DIR)/release
 	@-rm -rf $(TARGET_DIR)
-	@-rm -rf $(D)/kernel
-	@-rm -rf $(D)/*.do_compile
-	@-rm -rf $(D)/*.config.status
+	@-rm -rf $(DEPS_DIR)/kernel
+	@-rm -rf $(DEPS_DIR)/*.do_compile
+	@-rm -rf $(DEPS_DIR)/*.config.status
 	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 distclean:
@@ -29,7 +29,7 @@ distclean:
 	@-rm -rf $(TARGET_DIR)
 	@-rm -rf $(BUILD_DIR)
 	@-rm -rf $(SOURCE_DIR)
-	@-rm -rf $(D)
+	@-rm -rf $(DEPS_DIR)
 	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 # -----------------------------------------------------------------------------
