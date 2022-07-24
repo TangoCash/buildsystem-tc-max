6fedb581b2e139d07fa205e4619fbd2ec37fdf3e
max_10 <max_10@gmx.de>
2022-07-24 20:59:13 +0200

    - pugixml: clean up target

--- package/target/pugixml/pugixml.mk
+++ package/target/pugixml/pugixml.mk
@@ -12,5 +12,10 @@ PUGIXML_SITE = https://github.com/zeux/pugixml/releases/download/v$(PUGIXML_VERS
 PUGIXML_CONF_OPTS = \
 	-DBUILD_DEFINES="PUGIXML_HAS_LONG_LONG"
 
+define PUGIXML_TARGET_CLEANUP
+	rm -rf $(addprefix $(TARGET_LIB_DIR)/,cmake)
+endef
+PUGIXML_TARGET_CLEANUP_HOOKS += PUGIXML_TARGET_CLEANUP
+
 $(D)/pugixml: | bootstrap
 	$(call cmake-package)
