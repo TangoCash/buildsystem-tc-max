b5fe7cc01ee0172f15e49186027e5ba7ea47c5ba
max_10 <max_10@gmx.de>
2022-07-25 23:03:12 +0200

    - host-parted: update build

--- package/host/host-parted/host-parted.mk
+++ package/host/host-parted/host-parted.mk
@@ -1,14 +1,20 @@
 ################################################################################
 #
-# parted
+# host-parted
 #
 ################################################################################
 
+HOST_PARTED_AUTORECONF = YES
+
 HOST_PARTED_CONF_OPTS = \
 	--sbindir=$(HOST_DIR)/bin \
-	--without-readline \
-	--disable-debug \
-	--disable-device-mapper
+	--enable-static \
+	--disable-shared \
+	--disable-device-mapper \
+	--without-readline
+
+HOST_PARTED_MAKE_ENV = \
+	CFLAGS="$(HOST_CFLAGS) -fPIC"
 
 $(D)/host-parted: | bootstrap
 	$(call host-autotools-package)
