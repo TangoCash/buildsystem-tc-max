a1bd79036dca8b8ed7c34373871d6f6968ae1b48
max_10 <max_10@gmx.de>
2022-07-27 21:09:29 +0200

    - crosstool-ng: bump version to 7feb97f1

--- package/cross/crosstool-ng/crosstool-ng.mk
+++ package/cross/crosstool-ng/crosstool-ng.mk
@@ -14,7 +14,7 @@ CROSSTOOL_NG_DEPENDS = directories kernel.do_prepare
 CROSSTOOL_NG_CONFIG = crosstool-ng-$(TARGET_ARCH)-$(CROSSTOOL_GCC_VERSION)
 CROSSTOOL_NG_BACKUP = $(DL_DIR)/$(CROSSTOOL_NG_CONFIG)-kernel-$(KERNEL_VERSION)-backup.tar.gz
 
-CROSSTOOL_NG_CHECKOUT = ee635cdf
+CROSSTOOL_NG_CHECKOUT = 7feb97f1
 
 # -----------------------------------------------------------------------------
 
