32006dde4796a7feecef6e5af9f00ad444af4ce4
max_10 <max_10@gmx.de>
2022-08-14 16:23:28 +0200

    - host-ccache; update build

--- package/host/host-ccache/host-ccache.mk
+++ package/host/host-ccache/host-ccache.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CCACHE_VERSION = 2021-03-29
+HOST_CCACHE_VERSION = 2022-08-07
 CCACHE_DIR = local
 
 HOST_CCACHE_BIN    = /usr/bin/ccache
@@ -14,19 +14,19 @@ CCACHE_DIR = $(HOME)/.ccache-bs-$(TARGET_ARCH)-$(CROSSTOOL_GCC_VERSION)-kernel-$
 export CCACHE_DIR
 
 define HOST_CCACHE_LINK
-	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/cc; \
-	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/c++; \
-	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/gcc; \
+	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/cc
+	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/c++
+	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/gcc
 	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/g++
 endef
 
 define TARGET_CCACHE_LINK
-	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/$(GNU_TARGET_NAME)-gcc; \
+	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/$(GNU_TARGET_NAME)-gcc
 	ln -sf $(HOST_CCACHE_BIN) $(HOST_CCACHE_BINDIR)/$(GNU_TARGET_NAME)-g++
 endef
 
 $(D)/host-ccache: | directories
 	$(call STARTUP)
-	$(HOST_CCACHE_LINK)
-	$(TARGET_CCACHE_LINK)
+	$(call HOST_CCACHE_LINK)
+	$(call TARGET_CCACHE_LINK)
 	$(TOUCH)
