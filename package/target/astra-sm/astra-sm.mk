268a6efc7fb6ed8d15b019ee5f5ea60316a76095
max_10 <max_10@gmx.de>
2022-09-18 12:42:46 +0200

    - astra-sm: fix url

--- package/target/astra-sm/astra-sm.mk
+++ package/target/astra-sm/astra-sm.mk
@@ -7,14 +7,14 @@
 ASTRA_SM_VERSION = git
 ASTRA_SM_DIR = astra-sm.git
 ASTRA_SM_SOURCE = astra-sm.git
-ASTRA_SM_SITE = https://gitlab.com/crazycat69
+ASTRA_SM_SITE = https://github.com/crazycat69
 
 ASTRA_SM_DEPENDS = openssl
 
 ASTRA_SM_AUTORECONF = YES
 
 ifeq ($(GCC_VERSION),$(filter $(GCC_VERSION),11.3.0 12.1.0))
-ASTRA_SM_PATCH += 0004-replace-sys-siglist.patch-gcc
+ASTRA_SM_PATCH += 0002-replace-sys-siglist.patch-gcc
 endif
 
 ASTRA_SM_CONF_OPTS = \
